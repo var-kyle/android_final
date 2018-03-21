6f81fe89a94a186312929cdfd94d28e196c9cab6
var-kyle <34345435+var-kyle@users.noreply.github.com>
2018-03-20 21:56:22 -0400

    some commit

---
+++ AndroidFinal/app/src/main/java/com/afinal/group/best/androidfinal/OCTranspoMainActivity.java
@@ -0,0 +1,13 @@
+package com.afinal.group.best.androidfinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OCTranspoMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_octranspo_main);
+    }
+}
