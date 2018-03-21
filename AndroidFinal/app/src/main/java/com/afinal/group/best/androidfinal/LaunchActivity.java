ab70ff3d42cf1e62bafffe36c8e7a0c2cf5fd439
var-kyle <34345435+var-kyle@users.noreply.github.com>
2018-03-20 22:56:05 -0400

    added some stuff

--- AndroidFinal/app/src/main/java/com/afinal/group/best/androidfinal/LaunchActivity.java
+++ AndroidFinal/app/src/main/java/com/afinal/group/best/androidfinal/LaunchActivity.java
@@ -1,7 +1,10 @@
 package com.afinal.group.best.androidfinal;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LaunchActivity extends Activity {
 
@@ -9,5 +12,46 @@ public class LaunchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        //OC Transpo open handler
+        Button btn = this.findViewById(R.id.btnOCTranspo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LaunchActivity.this, OCTranspoMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Intake form open handler
+        btn = this.findViewById(R.id.btnIntake);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(LaunchActivity.this, IntakeFormMainActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        //Movie info open handler
+        btn = this.findViewById(R.id.btnMovieInfo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(LaunchActivity.this, MovieInfoMainActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+        //Multiple choice quiz open handler
+        btn = this.findViewById(R.id.btnMultipleChoice);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(LaunchActivity.this, MultipleChoiceMainActivity.class);
+                //startActivity(intent);
+            }
+        });
+
     }
 }
--- AndroidFinal/app/src/main/java/com/afinal/group/best/androidfinal/OCTranspoMainActivity.java
+++ AndroidFinal/app/src/main/java/com/afinal/group/best/androidfinal/OCTranspoMainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class OCTranspoMainActivity extends Activity {
 
+    private static final String APPID = "223eb5c3";
+    private static final String APIKEY = "ab27db5b435b8c8819ffb8095328e775";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
